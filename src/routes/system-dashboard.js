35b7c28b34d8e9340ee2379b99d575d1bf46a259
Ian Lucas Paim <ian.paim@hotmail.com>
2024-04-30 01:06:31 +0000

    feature: adiciona modal de selecao de status / detalhe do pedido

--- src/routes/system-dashboard.js
+++ src/routes/system-dashboard.js
@@ -33,6 +33,7 @@ router.get('/', async function (req, res, next) {
       beneficiado: 'Beneficiado',
       medicamento: 'Medicamento',
       status: 'Status',
+      buttons: 'Ações',
     };
   } else {
     tableHeaders = {
@@ -44,8 +45,88 @@ router.get('/', async function (req, res, next) {
     };
   }
 
+//   console.log('pedidosAll', pedidosAll);
+//   //renderiza a página
+//   res.render('system-dashboard', {
+//     user: req.user,
+//     title: 'Dashboard - CVBCXS dispensário',
+//     page: 'system-dashboard',
+//     bodyClass: 'table',
+//     system: true,
+//     prefix: 'system',
+//     data: {
+//       tableHeaders: tableHeaders,
+//       tableBody: pedidosAll,
+//       tablePageHeader: {
+//         title: 'Solicitações',
+//       },
+//     },
+//   });
+// });
+
+// module.exports = router;
+
+// const express = require('express');
+// const router = express.Router();
+// const jwt = require('jsonwebtoken');
+
+// // Função fictícia para simular a obtenção de dados de pedidos
+// function fetchOrdersData(userId, isAdmin) {
+//   // Supondo que você tenha dados fictícios de pedidos
+//   const pedidosAll = [
+//     {
+//       cim: '123',
+//       beneficiado: 'João',
+//       medicamento: 'Aspirina',
+//       status: 'pending',
+//     },
+//     {
+//       cim: '456',
+//       beneficiado: 'Maria',
+//       medicamento: 'Paracetamol',
+//       status: 'Processando',
+//     },
+//     {
+//       cim: '789',
+//       beneficiado: 'José',
+//       medicamento: 'Dipirona',
+//       status: 'completed',
+//     },
+//   ];
+//   return pedidosAll;
+// }
+
+// router.get('/', async function (req, res, next) {
+//   jwt.verify(req.cookies.token, req.cookies.secretKey, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).json({ message: 'Token inválido' });
+//     } else {
+//       req.user = decoded;
+//     }
+//   });
+
+//   // Simulando dados fictícios dos pedidos
+//   const pedidosAll = fetchOrdersData(req.user.id, req.user.admin);
+
+//   // Variáveis das colunas da tabela
+//   var tableHeaders = {
+//     cim: 'CIM',
+//     beneficiado: 'Beneficiado',
+//     medicamento: 'Medicamento',
+//     status: 'Status',
+//   };
+
+  // Se o usuário for administrador, adiciona a coluna de botões
+  if (req.user.admin) {
+    console.log('Usuário é administrador');
+    tableHeaders.buttons = 'Ações';
+  }
+
   console.log('pedidosAll', pedidosAll);
-  //renderiza a página
+
+  // Renderiza a página com os dados fictícios
+  console.log('user: ', req.user);
+  console.log('tableHeaders: ', tableHeaders);
   res.render('system-dashboard', {
     user: req.user,
     title: 'Dashboard - CVBCXS dispensário',
