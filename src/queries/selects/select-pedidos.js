9ebeb5f3e7eb93ef681c806d3bb0fdf93c94a4a4
Ian Lucas Paim <ian.paim@hotmail.com>
2024-05-12 01:15:33 +0000

    refactor: remove console.log desnecessário

--- src/queries/selects/select-pedidos.js
+++ src/queries/selects/select-pedidos.js
@@ -21,8 +21,6 @@ async function fetchOrdersFromDatabase(query) {
 }
 
 async function fetchOrdersData(userId, isAdmin = false) {
-  console.log('userId', userId);
-  console.log('isAdmin', isAdmin);
   let ordersFromDB = await fetchOrdersFromDatabase(
     isAdmin === true
       ? // ? `SELECT p.id AS pedido_id, p.nome_beneficiado, p.cim, p.id_loja, p.created_at AS pedido_criado_em, p.estado, STRING_AGG(DISTINCT CONCAT(m.medicamento, ' - ', m.composto), ', ') AS medicamentos_compostos FROM pedidos p JOIN pedidos_medicamentos pm ON p.id = pm.pedido_id JOIN medicamentos m ON pm.medicamento_id = m.id JOIN lojas l ON p.id_loja = l.id GROUP BY p.id, p.nome_beneficiado, p.cim, p.id_loja, p.created_at, p.estado ORDER BY p.id DESC;`
